83d2ab90b8effb368ce13f8dbda3ca69d2f7f047
joeyanovich <joedison.dias@gmail.com>
2024-04-19 21:38:06 -0300

    feat ✨: create filter by type and context

---
+++ capputeeno/src/components/filter-by-type.tsx
@@ -0,0 +1,64 @@
+"use client"
+
+import { useFilter } from "@/hooks/useFilter"
+import { FilterType } from "@/types/filter-types"
+import styled from "styled-components"
+
+interface FilterItemProps {
+  selected: boolean
+}
+
+const FilterList = styled.ul`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 2.5rem;
+
+  list-style: none;
+`
+
+const FilterItem = styled.li<FilterItemProps>`
+  font-family: inherit;
+  font-weight: ${props => props.selected ? '600' : '400'};
+  font-size: 1rem;
+  line-height: 1.375rem;
+  text-align: center;
+  text-transform: uppercase;
+  
+  cursor: pointer;
+
+  border-bottom: ${props => props.selected ? '4px solid var(--orange-low)' : ''};
+
+  color: var(--text-dark);
+`
+
+export function FilterByType() {
+  const { type, setType } = useFilter()
+
+  const handleChangeType = (value: FilterType) => {
+    setType(value)
+  }
+
+  return (
+    <FilterList>
+      <FilterItem
+        selected={type === FilterType.ALL}
+        onClick={() => handleChangeType(FilterType.ALL)}
+      >
+        Todos os produtos
+      </FilterItem>
+      <FilterItem
+        selected={type === FilterType.SHIRT}
+        onClick={() => handleChangeType(FilterType.SHIRT)}
+      >
+        Camisetas
+      </FilterItem>
+      <FilterItem
+        selected={type === FilterType.MUG}
+        onClick={() => handleChangeType(FilterType.MUG)}
+      >
+        Canecas
+      </FilterItem>
+    </FilterList>
+  )
+}
