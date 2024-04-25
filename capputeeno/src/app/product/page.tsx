3bbf0684278d46fd939d8dec6a20a944698dbb5a
joeyanovich <joedison.dias@gmail.com>
2024-04-24 21:57:23 -0300

    feat ✨: add item to cart

--- capputeeno/src/app/product/page.tsx
+++ capputeeno/src/app/product/page.tsx
@@ -5,6 +5,7 @@ import { DefaultPageLayout } from "@/components/default-page-layout"
 import { ShopBagIcon } from "@/components/icons/shopping-bag-icon"
 import { useProduct } from "@/hooks/useProducts"
 import { formatPrice } from "@/utils/format-price"
+import { Quantico } from "next/font/google"
 import styled from "styled-components"
 
 const Container = styled.div`
@@ -116,6 +117,27 @@ const ProductInfo = styled.div`
 export default function Product({ searchParams }: { searchParams: { id: string }}) {
   const { data } = useProduct(searchParams.id)
 
+  const handleAddToCart = () => {
+    let cartItems = localStorage.getItem('cart-items')
+    if (cartItems) {
+      let cartItemsArray = JSON.parse(cartItems)
+
+      let existingProductIndex = cartItemsArray.findIndex((item: { id: string }) => item.id === searchParams.id)
+
+      if (existingProductIndex != -1) {
+        cartItemsArray[existingProductIndex].quantity += 1;
+      } else {
+        cartItemsArray.push({ ...data, quantity: 1, id: searchParams.id })
+      }
+
+      localStorage.setItem('cart-items', JSON.stringify(cartItemsArray)) 
+    } else {
+      const newCart = [{ ...data, id: searchParams.id, quantity: 1 }]
+
+      localStorage.setItem('cart-items', JSON.stringify([newCart])) 
+    }
+  }
+
   console.log(data)
 
   return (
@@ -135,7 +157,7 @@ export default function Product({ searchParams }: { searchParams: { id: string }
                 <p>{data?.description}</p>
               </div>
             </ProductInfo>
-            <button>
+            <button onClick={handleAddToCart}>
               <ShopBagIcon />
               Adicionar ao carrinho
             </button>
